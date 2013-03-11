5aac7bed2756427e0db9b1bc9c3feaa0869d07a9
Dylan Barth <dylan.j.barth@gmail.com>
2013-03-11 08:30:50 -0700

    Pop up and warm up work

--- src/com/example/ballskills/Level1.java
+++ src/com/example/ballskills/Level1.java
@@ -1,6 +1,8 @@
 package com.example.ballskills;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
@@ -11,8 +13,18 @@ public class Level1 extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_level1);
-		View Level1View = new Level1View(this);
-		setContentView(Level1View);
+		// Alert Dialog 
+		AlertDialog.Builder alertDialog = new AlertDialog.Builder(this);
+		alertDialog.setTitle("Warm Up");
+		alertDialog.setMessage("Bounce around, have fun, prepare yo'self. Level 1 starts at 25.").setCancelable(false);
+		alertDialog.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+			@Override
+			public void onClick(DialogInterface dialog, int which) {
+				View level1View = new Level1View(Level1.this);
+				setContentView(level1View);
+			}
+		});
+		alertDialog.show();
 	}
 
 	@Override
